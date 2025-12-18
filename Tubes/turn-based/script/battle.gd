a0c9574f2c5656c7dc1031fa85ba5f6866707ace
Wildanwhyf <gwildan77@gmail.com>
2025-12-18 11:26:17 +0700

    +ui attack enemy

--- Tubes/turn-based/script/battle.gd
+++ Tubes/turn-based/script/battle.gd
@@ -1,4 +1,34 @@
 extends Control
 
+enum States {
+	OPTIONS,
+	TARGETS,
+}
+
+var state: States = States.OPTIONS
+
 @onready var _options: WindowDefault = $Options
 @onready var _options_menu: Menu = $Options/Options
+@onready var _enemies_menu: Menu = $Enemies
+
+func _ready() -> void:
+	_options_menu.button_focus(0)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		match state:
+			States.OPTIONS:
+				pass
+			States.TARGETS:
+				state = States.OPTIONS
+				_options_menu.button_focus()
+	
+func _on_options_button_focused(button: BaseButton) -> void:
+	pass
+	
+func _on_options_button_pressed(button: BaseButton) -> void:
+	match button.text:
+		"Attack":
+			state = States.TARGETS
+			_enemies_menu.button_focus()
+			
